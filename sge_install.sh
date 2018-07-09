db291f64d662a4d925e288ca9689300fee427389
furbelows <alden.huang@gmail.com>
2018-07-09 12:24:25 -0700

    Update sge_install.sh

--- sge_install.sh
+++ sge_install.sh
@@ -30,129 +30,38 @@ sudo service postfix stop
 sudo update-rc.d postfix disable
 
 ## change scheduler to allow rapid submits
-cat > ./grid <<EOL
-algorithm                         default
-schedule_interval                 0:0:1
-maxujobs                          0
-queue_sort_method                 load
-job_load_adjustments              np_load_avg=0.50
-load_adjustment_decay_time        0:7:30
-load_formula                      np_load_avg
-schedd_job_info                   true
-flush_submit_sec                  0
-flush_finish_sec                  0
-params                            none
-reprioritize_interval             0:0:0
-halftime                          168
-usage_weight_list                 cpu=1.000000,mem=0.000000,io=0.000000
-compensation_factor               5.000000
-weight_user                       0.250000
-weight_project                    0.250000
-weight_department                 0.250000
-weight_job                        0.250000
-weight_tickets_functional         0
-weight_tickets_share              0
-share_override_tickets            TRUE
-share_functional_shares           TRUE
-max_functional_jobs_to_schedule   200
-report_pjob_tickets               TRUE
-max_pending_tasks_per_job         50
-halflife_decay_list               none
-policy_hierarchy                  OFS
-weight_ticket                     0.500000
-weight_waiting_time               0.278000
-weight_deadline                   3600000.000000
-weight_urgency                    0.500000
-weight_priority                   0.000000
-max_reservation                   0
-default_duration                  INFINITY
-EOL
-sudo qconf -Msconf ./grid
-rm ./grid
+sudo qconf -Msconf erds_dnanexus/scheduler_defaults
 
 echo -e "group_name @allhosts\nhostlist NONE" > ./grid
 sudo qconf -Ahgrp ./grid
 rm ./grid
 
 ## add default queue
-cat > ./grid <<EOL
-qname                 all.q
-hostlist              @allhosts
-seq_no                0
-load_thresholds       NONE
-suspend_thresholds    NONE
-nsuspend              1
-suspend_interval      00:00:01
-priority              0
-min_cpu_interval      00:00:01
-processors            UNDEFINED
-qtype                 BATCH INTERACTIVE
-ckpt_list             NONE
-pe_list               make
-rerun                 FALSE
-slots                 2
-tmpdir                /tmp
-shell                 /bin/bash
-prolog                NONE
-epilog                NONE
-shell_start_mode      posix_compliant
-starter_method        NONE
-suspend_method        NONE
-resume_method         NONE
-terminate_method      NONE
-notify                00:00:01
-owner_list            NONE
-user_lists            NONE
-xuser_lists           NONE
-subordinate_list      NONE
-complex_values        NONE
-projects              NONE
-xprojects             NONE
-calendar              NONE
-initial_state         default
-s_rt                  INFINITY
-h_rt                  INFINITY
-s_cpu                 INFINITY
-h_cpu                 INFINITY
-s_fsize               INFINITY
-h_fsize               INFINITY
-s_data                INFINITY
-h_data                INFINITY
-s_stack               INFINITY
-h_stack               INFINITY
-s_core                INFINITY
-h_core                INFINITY
-s_rss                 INFINITY
-h_rss                 INFINITY
-s_vmem                INFINITY
-h_vmem                INFINITY
-EOL
-sudo qconf -Aq ./grid
-rm ./grid
+sudo qconf -Aq erds_dnanexus/queue_defaults
 
 QUEUE="all.q"
 HOSTNAME=`hostname`
 SLOTS=`grep -c "^processor" /proc/cpuinfo`
 
 # add to the execution host list
-TMPFILE=/tmp/sge.hostname-$HOSTNAME
+TMPFILE=./sge.hostname-$HOSTNAME
 echo -e "hostname $HOSTNAME\nload_scaling NONE\ncomplex_values NONE\nuser_lists NONE\nxuser_lists NONE\nprojects NONE\nxprojects NONE\nusage_scaling NONE\nreport_variables NONE" > $TMPFILE
 qconf -Ae $TMPFILE
 rm $TMPFILE
 
 # add to the all hosts list
-qconf -aattr hostgroup hostlist $HOSTNAME @allhosts
+sudo qconf -aattr hostgroup hostlist $HOSTNAME @allhosts
 
 # enable the host for the queue, in case it was disabled and not removed
-qmod -e $QUEUE@$HOSTNAME
+sudo qmod -e $QUEUE@$HOSTNAME
 
 # add slots
 if [ "$SLOTS" ]; then
-    qconf -aattr queue slots "[$HOSTNAME=$SLOTS]" $QUEUE
+    sudo qconf -aattr queue slots "[$HOSTNAME=$SLOTS]" $QUEUE
 fi
 
 # add submit host
-qconf -as `hostname`
+sudo qconf -as `hostname`
 sudo service gridengine-exec restart
 
 # should be correct
