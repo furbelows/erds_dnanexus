7ba9e6bfa9d9d2b631b6ec1d843aa34e753e69bb
furbelows <alden.huang@gmail.com>
2018-07-10 00:13:06 -0700

    Update sge_install.sh

--- sge_install.sh
+++ sge_install.sh
@@ -7,27 +7,28 @@
 sudo apt-get update -qq
 
 # configure the master host for SGE
-echo "gridengine-master shared/gridenginemaster string localhost" | sudo debconf-set-selections
+echo "gridengine-master shared/gridenginemaster string $(hostname)" | sudo debconf-set-selections
 echo "gridengine-master shared/gridenginecell string default" | sudo debconf-set-selections
 echo "gridengine-master shared/gridengineconfig boolean true" | sudo debconf-set-selections
 echo "postfix postfix/main_mailer_type select No configuration" | sudo debconf-set-selections
 
 # sge install
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y gridengine-common gridengine-master
-sleep 20
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y gridengine-client gridengine-exec
-sleep 20
 
 # don't need this shit
 sudo service postfix stop
 sudo update-rc.d postfix disable
 
+# add me to the sgeadmin
+sudo qconf -am dnanexus
+
 ## change scheduler to allow rapid submits
 sudo qconf -Msconf scheduler_defaults
 
-echo "group_name @allhosts\nhostlist NONE" > grid
-sudo qconf -Ahgrp grid
-rm grid
+echo "group_name @allhosts\nhostlist NONE" > /home/dnanexus/grid
+sudo qconf -Ahgrp /home/dnanexus/grid
+rm /home/dnanexus/grid
 
 ## add default queue
 sudo qconf -Aq queue_defaults
@@ -37,7 +38,9 @@ HOSTNAME=`hostname`
 SLOTS=`grep -c "^processor" /proc/cpuinfo`
 
 # add to the execution host list
-sudo qconf -Ae host_defaults
+echo "hostname $HOSTNAME\nload_scaling NONE\ncomplex_values NONE\nuser_lists NONE\nxuser_lists NONE\nprojects NONE\nxprojects NONE\nusage_scaling NONE\nreport_variables NONE" > /home/dnanexus/host_defaults
+sudo qconf -Ae /home/dnanexus/host_defaults
+rm /home/dnanexus/host_defaults
 
 # add to the all hosts list
 sudo qconf -aattr hostgroup hostlist $HOSTNAME @allhosts
@@ -53,7 +56,6 @@ fi
 # add submit host
 sudo qconf -as `hostname`
 sudo service gridengine-exec restart
-sleep 20
 
 # should be correct
 qstat -f 
